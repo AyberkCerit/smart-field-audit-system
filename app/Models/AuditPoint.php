9841d6f99a8ce5eb08812f981ce855b37880c518
AyberkCerit <a2407231005@gmail.com>
2026-07-29 15:07:02 +0300

    .toml removed

--- app/Models/AuditPoint.php
+++ app/Models/AuditPoint.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Spatie\Activitylog\Models\Concerns\LogsActivity;
+use Spatie\Activitylog\Traits\LogsActivity;
 use Spatie\Activitylog\LogOptions;
 
 class AuditPoint extends Model
